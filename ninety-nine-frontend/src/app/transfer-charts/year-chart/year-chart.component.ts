291b147749fa630a7ee650df93239261ecee248c
Jesus Tejedor <jesus.tejedor.gomez@gmail.com>
2020-08-26 11:38:41 +0200

    [feat] Added day chart component to view transfer chart using year as unit

--- ninety-nine-frontend/src/app/transfer-charts/year-chart/year-chart.component.ts
+++ ninety-nine-frontend/src/app/transfer-charts/year-chart/year-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GenericChartData, DataSize } from '../generic-chart/generic-chart.component';
+import { YearTransferViewerService } from './service/year-transfer-viewer.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-year-chart',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YearChartComponent implements OnInit {
 
-  constructor() { }
+  chartData: GenericChartData;
+
+  constructor(private yearTransferViewer: YearTransferViewerService) {
+    this.chartData = {
+      title: "Transfer per year",
+      description: "View transfer received per year",
+      dataSizeLabel: ["Last Two Year","Last Three Year","Last Five Year"],
+      view: [400,400],
+      data: [],
+      // options
+      showXAxis:true,
+      showYAxis:true,
+      gradient:false,
+      showXAxisLabel:true,
+      xAxisLabel:"Years",
+      showYAxisLabel:true,
+      yAxisLabel:"Transfers",
+      colorScheme: {
+        domain: ['#4E18C5']
+      }
+    }
+  }
 
   ngOnInit() {
+    this.changePeriod(DataSize.SMALL)
+  }
+
+  changePeriod(size: DataSize){
+    if(size === DataSize.SMALL){
+      this.yearTransferViewer.getYearDataLast2Year().pipe(first()).subscribe(
+        dataOneMonth => {
+          this.chartData.data = dataOneMonth
+        }
+      )
+    }
+    else if(size === DataSize.MEDIUM){
+      this.yearTransferViewer.getYearDataLast3Year().pipe(first()).subscribe(
+        dataThreeMonth => {
+          this.chartData.data = dataThreeMonth
+        }
+      )
+    }
+    else if(size === DataSize.LARGE){
+      this.yearTransferViewer.getYearDataLast5Year().pipe(first()).subscribe(
+        dataSixMonth => {
+          this.chartData.data = dataSixMonth
+        }
+      )
+    }
+  }
+
+  periodChange(event){
+    this.changePeriod(DataSize[event.value as string])
   }
 
 }
