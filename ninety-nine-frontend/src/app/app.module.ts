38a03edb6d768fece3e9af66d9d0202bf04f4b31
Jesus Tejedor <jesus.tejedor.gomez@gmail.com>
2020-08-26 11:39:01 +0200

    [feat] Added all chart components to main app

--- ninety-nine-frontend/src/app/app.module.ts
+++ ninety-nine-frontend/src/app/app.module.ts
@@ -5,25 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { DayChartComponent } from './transfer-charts/day-chart/day-chart.component';
-import { MonthChartComponent } from './transfer-charts/month-chart/month-chart.component';
 import { YearChartComponent } from './transfer-charts/year-chart/year-chart.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TransferPerDayService } from './services/transfer/day/transfer-per-day.service';
-import { TransferPerMonthService } from './services/transfer/transfer-per-month.service';
-import { TransferPerYearService } from './services/transfer/transfer-per-year.service';
-import { DataToViewConvertService } from './services/data-to-view-convert.service';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { DayTransferViewerService } from './services/transfer/day/day-transfer-viewer.service';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { DayTransferViewerService } from './transfer-charts/day-chart/service/day-transfer-viewer.service';
+import { GenericChartComponent } from './transfer-charts/generic-chart/generic-chart.component';
+import { ChartHttpProviderService } from './transfer-charts/service/chart-http-provider.service';
+import { YearTransferViewerService } from './transfer-charts/year-chart/service/year-transfer-viewer.service';
+import { MonthChartComponent } from './transfer-charts/month-chart/month-chart.component';
+import { MonthTransferViewerService } from './transfer-charts/month-chart/service/month-transfer-viewer.service';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     DayChartComponent,
-    MonthChartComponent,
-    YearChartComponent
+    YearChartComponent,
+    GenericChartComponent,
+    MonthChartComponent
   ],
   imports: [
     BrowserModule,
@@ -33,14 +35,14 @@ import {MatGridListModule} from '@angular/material/grid-list';
     NgxChartsModule,
     MatButtonToggleModule,
     MatExpansionModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule
   ],
   providers: [
-    TransferPerDayService,
-    TransferPerMonthService,
-    TransferPerYearService,
-    DataToViewConvertService,
-    DayTransferViewerService
+    DayTransferViewerService,
+    ChartHttpProviderService,
+    YearTransferViewerService,
+    MonthTransferViewerService
   ],
   bootstrap: [AppComponent]
 })
