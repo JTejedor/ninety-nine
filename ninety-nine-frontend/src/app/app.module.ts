bb3a4c7bdece270825bfc8b26b585b4a7f168ce3
Jesus Tejedor <jesus.tejedor.gomez@gmail.com>
2020-08-26 14:36:36 +0200

    [refactor] Added new components to global application

--- ninety-nine-frontend/src/app/app.module.ts
+++ ninety-nine-frontend/src/app/app.module.ts
@@ -8,22 +8,28 @@ import { DayChartComponent } from './transfer-charts/day-chart/day-chart.compone
 import { YearChartComponent } from './transfer-charts/year-chart/year-chart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { DayTransferViewerService } from './transfer-charts/day-chart/service/day-transfer-viewer.service';
 import { GenericChartComponent } from './transfer-charts/generic-chart/generic-chart.component';
 import { ChartHttpProviderService } from './transfer-charts/service/chart-http-provider.service';
 import { YearTransferViewerService } from './transfer-charts/year-chart/service/year-transfer-viewer.service';
 import { MonthChartComponent } from './transfer-charts/month-chart/month-chart.component';
 import { MonthTransferViewerService } from './transfer-charts/month-chart/service/month-transfer-viewer.service';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { GenericMonthHitComponent } from './month-hits/generic-month-hit/generic-month-hit.component';
 import { GenericMonthHitService } from './month-hits/generic-month-hit.service';
 import { MaxTransferHitComponent } from './month-hits/max-transfer-hit/max-transfer-hit.component';
 import { MaxTransferHitService } from './month-hits/max-transfer-hit/max-transfer-hit.service';
 import { MaxAmountHitComponent } from './month-hits/max-amount-hit/max-amount-hit.component'
-import { MaxAmountHitService } from './month-hits/max-amount-hit/max-amount-hit.service'
+import { MaxAmountHitService } from './month-hits/max-amount-hit/max-amount-hit.service';
+import { MatSelectModule } from '@angular/material/select';
+import { IbanSelectorComponent } from './iban-transfer/iban-selector/iban-selector.component'
+import { IbanSelectorService } from './iban-transfer/iban-selector/iban-selector.service';
+import { IbanLastTransfersViewComponent } from './iban-transfer/iban-last-transfers-view/iban-last-transfers-view.component'
+import { MatTableModule } from '@angular/material/table'
+import { IbanLastTransfersViewService } from './iban-transfer/iban-last-transfers-view/iban-last-transfers-view.service'
 
 @NgModule({
   declarations: [
@@ -34,7 +40,9 @@ import { MaxAmountHitService } from './month-hits/max-amount-hit/max-amount-hit.
     MonthChartComponent,
     GenericMonthHitComponent,
     MaxTransferHitComponent,
-    MaxAmountHitComponent
+    MaxAmountHitComponent,
+    IbanSelectorComponent,
+    IbanLastTransfersViewComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +53,9 @@ import { MaxAmountHitService } from './month-hits/max-amount-hit/max-amount-hit.
     MatButtonToggleModule,
     MatExpansionModule,
     MatGridListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatSelectModule,
+    MatTableModule
   ],
   providers: [
     DayTransferViewerService,
@@ -54,7 +64,9 @@ import { MaxAmountHitService } from './month-hits/max-amount-hit/max-amount-hit.
     MonthTransferViewerService,
     GenericMonthHitService,
     MaxTransferHitService,
-    MaxAmountHitService
+    MaxAmountHitService,
+    IbanSelectorService,
+    IbanLastTransfersViewService
   ],
   bootstrap: [AppComponent]
 })
